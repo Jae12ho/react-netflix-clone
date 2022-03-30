4085cc08d1713d843a4e8ee55361de4db7c7011c
Jaeho Yang <ho4121e@gmail.com>
2022-03-30 23:54:56 +0900

    Chore: axios 수정

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
     baseURL: "https://api.themoviedb.org/3",
     params: {
-        api_key: "2a1924446876d09b8e5888d6bb8fa78c",
+        api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
         language: "ko-KR",
     },
 });
